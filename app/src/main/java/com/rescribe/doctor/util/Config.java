c541461f757681b6b36cd1fcd933dcf4fba1a870
Ritesh P <ritesh.pandhurkar@scorgtechnologies.com>
2018-04-16 16:56:59 +0530

    removed extra space in URL

--- app/src/main/java/com/rescribe/doctor/util/Config.java
+++ app/src/main/java/com/rescribe/doctor/util/Config.java
@@ -37,7 +37,7 @@ public class Config {
     //  public static String BASE_URL = "http://rescribe.in:3003/"; // production
     //  public static String BASE_URL = "http://172.16.100.219:3003/"; // add local IP
     //  public static String BASE_URL = "http://192.168.0.182:3003/";
-    public static String BASE_URL = " http://drrescribe.com:3003/"; // development
+    public static String BASE_URL = "http://drrescribe.com:3003/"; // development
 
     public static final String FORGOT_PASSWORD_URL = "https://rescribe.in/app.html#/access/forgotPassword";
     public static final String ADD_NEW_PATIENT_WEB_URL = "https://rescribe.in/app.html#/addpatientmobile/";
